fd2f2d9618bad6fa7b8b82e0edffe8495032210d
Matibox <mateusz.hladky@gmail.com>
2025-12-08 09:52:15 +0100

    Finish day 2

--- day_2/solution.py
+++ day_2/solution.py
@@ -1,8 +1,38 @@
-def part1(lines: list[str]):
+def part1(lines: 'list[str]'):
+  line = lines[0]
 
-    return None
+  sum = 0
+
+  for id_group in line.split(','):
+    [start, end] = [int(id) for id in id_group.split('-')]
+      
+    for _id in range(start, end + 1):
+      id = str(_id)
+         
+      if len(id) % 2 == 1:
+        continue
+         
+      if id[0:int(len(id)/2)] == id[int(len(id)/2):]:
+        sum += _id
+
+  return sum
 
 
-def part2(lines: list[str]):
+def is_repeated_subsequence(s: 'str') -> 'bool':
+  return s in (s + s)[1:-1]
 
-    return None
+def part2(lines: 'list[str]'):
+  line = lines[0]
+
+  sum = 0
+
+  for id_group in line.split(','):
+    [start, end] = [int(id) for id in id_group.split('-')]
+      
+    for _id in range(start, end + 1):
+      id = str(_id)
+         
+      if is_repeated_subsequence(id):
+        sum += _id
+
+  return sum
