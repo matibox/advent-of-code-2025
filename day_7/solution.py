9bbd7aee4cbe8ebcc79ba6becbcb27770c849132
Matibox <mateusz.hladky@gmail.com>
2025-12-21 13:19:03 +0100

    Finish day 7

--- day_7/solution.py
+++ day_7/solution.py
@@ -1,8 +1,42 @@
-def part1(lines: list[str]):
+from functools import reduce
 
-    return None
+def part1(lines: 'list[str]'):
+  splits = 0
+  state = []
+
+  for ch in lines[0]:
+    if ch == 'S':
+      state.append(1)
+    else:
+      state.append(0)
+
+  for line in lines[1:]:
+    for x, ch in enumerate(line):
+      if ch == '^' and state[x] == 1:
+        splits += 1
+        state[x] = 0
+        state[x - 1] = 1
+        state[x + 1] = 1
+
+  return splits
 
 
-def part2(lines: list[str]):
+def part2(lines: 'list[str]'):
+  state = []
 
-    return None
+  for ch in lines[0]:
+    if ch == 'S':
+      state.append(1)
+    else:
+      state.append(0)
+
+  for line in lines[1:]:
+    for x, ch in enumerate(line):
+      if ch == '^' and state[x] > 0:
+        state[x - 1] += state[x]
+        state[x + 1] += state[x]
+        state[x] = 0
+
+    # print(' '.join(str(c) for c in state))
+
+  return reduce(lambda a, b: a + b, state)
